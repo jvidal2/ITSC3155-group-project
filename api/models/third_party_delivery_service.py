58bff2f263f28361263d34306860fabe4b921af2
DijonayDawson <147894815+DijonayDawson@users.noreply.github.com>
2025-04-13 21:59:28 -0400

    Create third_party_delivery_service in models
    Create third_party_delivery_service in models and updated the code

---
+++ api/models/third_party_delivery_service.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from ..dependencies.database import Base
+
+class ThirdPartyDeliveryService(Base):
+    __tablename__ = "third_party_delivery_service"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+    amount = Column(DECIMAL(6, 2), nullable=False, default='0.00')
+    status = Column(String(50), nullable=False)
