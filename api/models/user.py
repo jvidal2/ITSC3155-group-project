b1fbd01705273c191c375d39bd1eea55529ae431
Alice Chen <achen24@charlotte.edu>
2025-04-13 21:50:09 -0400

    updated user model

--- api/models/user.py
+++ api/models/user.py
@@ -8,7 +8,7 @@ class User(Base):
     __tablename__ = "user"
 
     user_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    name = Column(String(100), unique=True, nullable=True)
+    name = Column(String(100), nullable=True)
     email = Column(String(100), unique=True, nullable=True)
     phone_number = Column(Integer, unique=True, nullable=True)
 
