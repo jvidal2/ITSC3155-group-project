c5c02a86dffda4a7e7f2cdc6171a999f9667d429
Alice Chen <achen24@charlotte.edu>
2025-04-14 19:23:22 -0400

    no longer require unique review text

--- api/models/reviews.py
+++ api/models/reviews.py
@@ -9,7 +9,7 @@ class Reviews(Base):
 
     customer_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     rating = Column(Integer, unique=True, nullable=True)
-    review_text = Column(String(100), unique=True, nullable=True)
+    review_text = Column(String(100), nullable=True)
     image_included = Column(Boolean, unique=True, nullable=True)
 
     user = relationship("User", back_populates="reviews")
