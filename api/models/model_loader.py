636703eaddebbfd4d8c1793cae1e872bfb9a29d1
Alice Chen <achen24@charlotte.edu>
2025-04-13 22:06:14 -0400

    added user and reviews

--- api/models/model_loader.py
+++ api/models/model_loader.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, recipes, sandwiches, resources, menu_item
+from . import orders, order_details, recipes, sandwiches, resources, payment, menu_item, user, reviews
 from . import payments
 from ..dependencies.database import engine
 
@@ -9,5 +9,7 @@ def index():
     recipes.Base.metadata.create_all(engine)
     sandwiches.Base.metadata.create_all(engine)
     resources.Base.metadata.create_all(engine)
-    payments.Base.metadata.create_all(engine)
+    payment.Base.metadata.create_all(engine)
     menu_item.Base.metadata.create_all(engine)
+    user.Base.metadata.create_all(engine)
+    reviews.Base.metadata.create_all(engine)
