baa57d4ecb7920d0c8645ab3d60ee01ab0bf2cc6
DijonayDawson <147894815+DijonayDawson@users.noreply.github.com>
2025-04-13 21:40:34 -0400

    created administration in schemas
    created administration in schemas and added code

---
+++ api/schemas/administration.py
@@ -0,0 +1,22 @@
+from typing import Optional
+from pydantic import BaseModel
+
+class AdministrationBase(BaseModel):
+    status: str
+    name: str
+
+
+class AdministrationCreate(AdministrationBase):
+    pass
+
+
+class AdministrationUpdate(BaseModel):
+    status: Optional[str] = None
+    name: Optional[str] = None
+
+
+class Administration(AdministrationBase):
+    admin_ID: int
+
+    class Config:
+        from_attributes = True
