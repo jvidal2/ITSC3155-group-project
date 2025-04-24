7d58d4efda87c6504c8d6c5289344f3919ae6021
Trinity <thaot12093@gmail.com>
2025-04-24 12:38:55 -0400

    update: status column of models/staff.py

--- api/models/staff.py
+++ api/models/staff.py
@@ -8,8 +8,8 @@ class Staff(Base):
     __tablename__ = "staff"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    name = Column(String(100), unique=True, nullable=True)
-    status = Column(String, nullable=False, server_default='0.0')
+    name = Column(String(100), unique=True, nullable=False)
+    status = Column(String, nullable=False, server_default='inactive')
     order_id = Column(Integer, ForeignKey("order_details.id"))
 
     order_details = relationship("OrderDetail", back_populates="staff")
