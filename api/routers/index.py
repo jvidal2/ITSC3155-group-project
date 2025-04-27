8a595aad9b403cb20a211110cdeaa35adf2f1575
seriksou <seriksou@charlotte.edu>
2025-04-27 13:23:32 -0400

    feat: add sandwiches router and connect to app

--- api/routers/index.py
+++ api/routers/index.py
@@ -1,4 +1,4 @@
-from . import orders, order_details
+from . import orders, order_details, administration, resources, sandwiches
 
 
 def load_routes(app):
@@ -6,3 +6,5 @@ def load_routes(app):
     app.include_router(order_details.router)
     app.include_router(administration.router)
     app.include_router(resources.router)
+    app.include_router(sandwiches.router)
+
