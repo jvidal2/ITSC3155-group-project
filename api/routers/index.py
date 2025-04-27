fe7f0278b2ae60fbeabad27881f6a9640c0a5c9d
Trinity <thaot12093@gmail.com>
2025-04-27 15:31:01 -0400

    added staff and delivery connections

--- api/routers/index.py
+++ api/routers/index.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, administration, resources, sandwiches, payment, third_party_delivery_service, reviews, recipes, user
+from . import orders, order_details, administration, resources, sandwiches, payment, third_party_delivery_service, reviews, recipes, user, staff, delivery
 
 
 def load_routes(app):
@@ -12,5 +12,7 @@ def load_routes(app):
     app.include_router(reviews.router)
     app.include_router(recipes.router)
     app.include_router(user.router)
+    app.include_router(staff.router)
+    app.include_router(delivery.router)
 
 
