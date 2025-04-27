7142df5027910c996491362b9a88a965eac4af52
seriksou <seriksou@charlotte.edu>
2025-04-27 16:27:25 -0400

    fix: correct import promotions in promotions controller

--- api/controllers/promotions.py
+++ api/controllers/promotions.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status, Response
-from ..models import promotion as model
+from ..models import promotions as model
 from sqlalchemy.exc import SQLAlchemyError
 
 
