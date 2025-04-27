b7c07c842c01ab48be5913febd0361b4949b8b62
Alice Chen <achen24@charlotte.edu>
2025-04-27 15:13:41 -0400

    updated index.py

--- api/routers/index.py
+++ api/routers/index.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, administration, resources, sandwiches
+from . import orders, order_details, administration, resources, sandwiches, payment, third_party_delivery_service
 
 
 def load_routes(app):
@@ -7,4 +7,7 @@ def load_routes(app):
     app.include_router(administration.router)
     app.include_router(resources.router)
     app.include_router(sandwiches.router)
+    app.include_router(payment.router)
+    app.include_router(third_party_delivery_service.router)
+
 
