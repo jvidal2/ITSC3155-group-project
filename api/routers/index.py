47bf2cbc6788a6401bbc108fa92c79420c996cb4
Jaz <jazminvidal91@gmail.com>
2025-04-27 15:36:24 -0400

    updated index.py

--- api/routers/index.py
+++ api/routers/index.py
@@ -1,4 +1,4 @@
-from . import orders, order_details, administration, resources, sandwiches, payment, third_party_delivery_service, reviews, recipes, user, staff, delivery
+from . import orders, order_details, administration, resources, sandwiches, payment, third_party_delivery_service, reviews, recipes, user, staff, delivery, menu_item
 
 
 def load_routes(app):
@@ -14,5 +14,6 @@ def load_routes(app):
     app.include_router(user.router)
     app.include_router(staff.router)
     app.include_router(delivery.router)
+    app.include_router(menu_item.router)
 
 
