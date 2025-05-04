9d96da60223986aee513101d226e276997810585
Trinity <thaot12093@gmail.com>
2025-05-04 13:50:13 -0400

    update: change table variables to reflect user story

--- api/models/order_pickup.py
+++ api/models/order_pickup.py
@@ -8,4 +8,7 @@ class OrderPickup(Base):
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String(100), nullable=False)
-    address = Column(String(255), nullable=False)
+    order_id = Column(Integer, ForeignKey("orders.id"))
+    description = Column(String, ForeignKey("orders.description"))
+    amount = Column(DECIMAL, ForeignKey("payment.amount"))
+    payment_method = Column(String, ForeignKey("payment.method"))
