a56842636b6fa3be0bfc53708d066a9637f16e04
Trinity <thaot12093@gmail.com>
2025-05-04 12:24:13 -0400

    update: changed delivery.py to order_pickup.py

--- api/routers/order_pickup.py
+++ api/routers/order_pickup.py
@@ -1,32 +1,32 @@
 from fastapi import APIRouter, Depends, FastAPI, status, Response
 from sqlalchemy.orm import Session
-from ..controllers import delivery as controller
-from ..schemas import delivery as schema
+from ..controllers import order_pickup as controller
+from ..schemas import order_pickup as schema
 from ..dependencies.database import engine, get_db
 
 router = APIRouter(
-    tags=['Delivery'],
-    prefix="/Delivery"
+    tags=['Order Pickup'],
+    prefix="/Order-pickup"
 )
 
 
-@router.post("/", response_model=schema.Delivery)
-def create(request: schema.DeliveryCreate, db: Session = Depends(get_db)):
+@router.post("/", response_model=schema.OrderPickup)
+def create(request: schema.OrderPickupCreate, db: Session = Depends(get_db)):
     return controller.create(db=db, request=request)
 
 
-@router.get("/", response_model=list[schema.Delivery])
+@router.get("/", response_model=list[schema.OrderPickup])
 def read_all(db: Session = Depends(get_db)):
     return controller.read_all(db)
 
 
-@router.get("/{item_id}", response_model=schema.Delivery)
+@router.get("/{item_id}", response_model=schema.OrderPickup)
 def read_one(item_id: int, db: Session = Depends(get_db)):
     return controller.read_one(db, item_id=item_id)
 
 
-@router.put("/{item_id}", response_model=schema.Delivery)
-def update(item_id: int, request: schema.DeliveryUpdate, db: Session = Depends(get_db)):
+@router.put("/{item_id}", response_model=schema.OrderPickup)
+def update(item_id: int, request: schema.OrderPickupUpdate, db: Session = Depends(get_db)):
     return controller.update(db=db, request=request, item_id=item_id)
 
 
