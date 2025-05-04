2a905f09898543564e300686c9dab07b2c778adb
Trinity <thaot12093@gmail.com>
2025-05-04 12:23:41 -0400

    update: changed database name from sandwich_maker_api to restaurant_ordering_system

--- api/dependencies/config.py
+++ api/dependencies/config.py
@@ -3,6 +3,6 @@ class conf:
     db_name = "restaurant_ordering_system"
     db_port = 3306
     db_user = "root"
-    db_password = "M@Rk3r$--)*=+"
+    db_password = "changeme"
     app_host = "localhost"
     app_port = 8000
