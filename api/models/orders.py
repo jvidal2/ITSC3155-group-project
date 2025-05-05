69bb7d6c6bcb654a45c74fcb04df8671b0222cdf
Alice Chen <achen24@charlotte.edu>
2025-05-04 23:04:14 -0400

    updated tracking relationship to orders

--- api/models/orders.py
+++ api/models/orders.py
@@ -13,3 +13,4 @@ class Order(Base):
 
     order_details = relationship("OrderDetail", back_populates="order")
     payment = relationship("Payment", back_populates="order")
+    tracking = relationship("OrderTracking", back_populates="order", uselist=False)
