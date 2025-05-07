9688003de8783f0e45b739f77d302cd598144434
Trinity <thaot12093@gmail.com>
2025-05-06 22:15:52 -0400

    update: added address

--- api/models/third_party_delivery_service.py
+++ api/models/third_party_delivery_service.py
@@ -9,6 +9,7 @@ class ThirdPartyDeliveryService(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String(100), nullable=False)
     amount = Column(DECIMAL(6, 2), nullable=False, default='0.00')
+    address = Column(String(100), nullable=False)
     status = Column(String(50), nullable=False)
 
     order_details = relationship("OrderDetail", back_populates="third_party_delivery_service")
